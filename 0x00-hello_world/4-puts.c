4f74fd6e98eecf6066105a5471ada912e835e9c2
zanokuhlemaziya <zaza.zulu9@gmail.com>
2023-06-15 17:29:51 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints "Programming is like building a multingual puzzle, followed by by a newline."
+ * return0.
+ */
+
+int main(void)
+{
+	puts("\"Ptograming is like building a multingual puzzle, followed by a newline"); 
+	return (0);
+}
