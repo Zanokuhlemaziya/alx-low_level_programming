85104982af2c899eb561c2d5bb7925d2143cd466
zanokuhlemaziya <zaza.zulu9@gmail.com>
2023-06-15 18:24:31 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints "with proper grammar, but the outcomec is a piece of art," followed newline
+ * Rrturn: always 0.
+ *
+ */
+
+init main(void)
+
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	retutn (0);
+}
