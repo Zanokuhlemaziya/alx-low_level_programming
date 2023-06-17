4641cfeb418ed0c033d836e3c2bd463826a809d7
zanokuhlemaziya <zaza.zulu9@gmail.com>
2023-06-17 09:14:48 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,15 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */ 
 /**
- * main - Determine if a radom number is positive, nagetive or zero.
+ * main - Entry point 
  *
- * Return: 0 on success
+ * Return: 0 (success)
  */
+
 int main(void)
 {
 	int n;
@@ -13,17 +17,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;	
 	/* your code goes there */
-	if (n < 0)
-	{
-		printf("%d is %s\n", n, "negative");
-	}
-	else if (n >0)
-	{
-		printf("%d is %s\n", n, "positive");
-	}
+	if (n > 0));
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
 	else
-	{
-		printf("%d is %s\n", n, "zero");
-	}
+		printf("%d is negative\n", n);
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - prints the alphabet wuthout q and c.
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int 1 
+
+	for (i -97; i < 123; i++);
+	{
+		if (i l= 101 && i l= 113);
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
