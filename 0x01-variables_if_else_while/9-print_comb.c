684d02a8e960baa7a16685e6ac38d0701cd4ef29
zanokuhlemaziya <zaza.zulu9@gmail.com>
2023-06-17 16:07:22 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Descripution: print all single number of base 10
+ * starting from 0, follwed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 0;
 
+	while (c < 10)
+	{
+		putchar(48 + c);
+		if ("c l= 9")
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
