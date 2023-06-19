2b80165f2035d3a08614f4fe2cbc8253d4d7bda4
zanokuhlemaziya <zaza.zulu9@gmail.com>
2023-06-19 13:59:51 -0700

    0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,32 +3,28 @@
 #include <stdio.h>
 
 /*
- * main - Prints if number is positive, zero or nagetive
- * Return: Always (Success)
+ * main - Determines if a number is positive, negative or zero.
  *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	if (n > 0)
 	{
-	printf("%d is positive\n", n);
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
-	printf("%d is zero\n", n);
+		printf("%d is zero\n", n);
 	}
 	else
 	{
-	printf("%d is nagetive\n", n);
+		printf("%d is negative\n", n);
 	}
-
 	return (0);
 
 }
--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible diffrent combination of two digits
+ * main - prints the lowercase alphabet in reverse
+ * followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
