e2ec893fb3b2a00c3b974433f6502f83d3da3142
zanokuhlemaziya <zaza.zulu9@gmail.com>
2023-06-21 12:29:28 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,19 +3,19 @@
 /**
  * print_sign - Prints the sign of a number
  * @n: The number to be checked
- * Return: 1 fir positive num, -1 negative num or zero for anything else
+ * Return: 1 for positive num, -1 for negative num or zero for anything alse
  */
 int print_sign(int n)
 {
-	if (n > 0 )
+	if (n > 0)
 	{
-	_putchar(45);
-	return (1);
+	_putchar(43);
+	return(1);
 	}
 	else if (n < 0)
 	{
-	_putchar(43);
-	 return (-1);
+	_putchar(45);
+	return (-1);
 	}
 	else
 	{
