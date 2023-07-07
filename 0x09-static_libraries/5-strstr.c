9301c1632266c3d27fa1239a14682500dd93462b
zanokuhlemaziya <zaza.zulu9@gmail.com>
2023-07-07 06:59:17 -0700

    update

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -12,12 +12,12 @@ char *_strstr(char *haystack, char *needle)
 {
 	char *bhaystack;
 	char *pneedle;
-	
+
 	while (*haystack != '\0')
 	{
 		bhaystack = haystack;
 		pneedle = needle;
-		
+
 		while (*haystack != '\0' && *pneedle != '\0' && *haystack == *pneedle)
 		{
 			haystack++;
