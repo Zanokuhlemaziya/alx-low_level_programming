457fb4954ecda016c6c0df62fc03b85dfaa4f0ab
Zanokuhlemaziya <zaza.zulu9@gmail.com>
2023-07-31 14:31:00 -0700

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -5,7 +5,6 @@
  * get_nodeint_at_index - Find a node in a list.
  * @head: Address of the first node in a list.
  * @index: Position of a the node to find (starting from 0).
- * 
  * Return: Node address.
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
